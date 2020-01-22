746f219b41dd8e8fed1d0589e93adb4de10a0693
Dominique Nguyen <domnguyen@gmail.com>
2020-01-22 14:33:16 -0800

    create structure of store.js and add store object to main

--- src/main.js
+++ src/main.js
@@ -6,7 +6,8 @@ import store from './store'
 Vue.config.productionTip = false
 
 new Vue({
+  el: '#app',
   router,
   store,
-  render: function (h) { return h(App) }
-}).$mount('#app')
+  render: h => h(App)
+})
