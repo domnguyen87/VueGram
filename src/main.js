922cdadad41c203c9b40b086a2257b2b7186a80d
Dominique Nguyen <domnguyen@gmail.com>
2020-01-23 11:56:53 -0800

    create firebase

--- src/main.js
+++ src/main.js
@@ -2,12 +2,19 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+const fb = require('./firebaseConfig.js')
+
 
 Vue.config.productionTip = false
 
-new Vue({
-  el: '#app',
-  router,
-  store,
-  render: h => h(App)
+let app
+fb.auth.onAuthStateChanged(user => {
+  if (!app) {
+    app = new Vue({
+      el: '#app',
+      router,
+      store,
+      render: h => h(App)
+    })
+  }
 })
