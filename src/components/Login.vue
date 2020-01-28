68609e0cf6f89a1ab9d0a4f6556b8849ce01a574
Dominique Nguyen <domnguyen@gmail.com>
2020-01-28 13:17:58 -0800

    create sign-up logic

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -23,6 +23,28 @@
                         <a>Create an Account</a>
                     </div>
                 </form>
+                <form v-if="!showLoginForm && !showForgotPassword" @submit.prevent>
+                    <h1>Get Started</h1>
+
+                    <label for="name">Name</label>
+                    <input v-model.trim="signupForm.name" type="text" placeholder="Savvy Apps" id="name" />
+
+                    <label for="title">Title</label>
+                    <input v-model.trim="signupForm.title" type="text" placeholder="Company" id="title" />
+
+                    <label for="email2">Email</label>
+                    <input v-model.trim="signupForm.email" type="text" placeholder="you@email.com" id="email2" />
+
+                    <label for="password2">Password</label>
+                    <input v-model.trim="signupForm.password" type="password" placeholder="min 6 characters" id="password2" />
+
+                    <button @click="signup" class="button">Sign Up</button>
+
+                    <div class="extras">
+                        <a @click="toggleForm">Back to Log In</a>
+                    </div>
+                </form>
+
             </div>
         </section>
     </div>
@@ -37,6 +59,12 @@
                 loginForm: {
                     email:'',
                     password: ''
+                },
+                signupForm: {
+                    name:'',
+                    title:'',
+                    email:'',
+                    password:''
                 }
             }
         },
