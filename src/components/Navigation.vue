87e80cfb6cf952a774a8e2c8a4fdc6cdc65995e2
Dominique Nguyen <domnguyen@gmail.com>
2020-01-30 13:25:24 -0800

    Update markup on Navigation

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,7 +1,16 @@
 <template>
-    <div>
-        
-    </div>
+    <header>
+        <section>
+            <div class="col1">
+                <router-link to="dashboard"><h3>Vuegram</h3></router-link>
+                <ul class='inline'>
+                    <li><router-link to="dashboard">Dashboard</router-link></li>
+                    <li><router-link to="settings">Settings</router-link></li>
+                    <li><a @click="logout">Logout</li>
+                </ul>
+            </div>
+        </section>
+    </header>
 </template>
 
 <script>
