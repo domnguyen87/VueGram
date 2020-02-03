8a397e90e9021e10e179ca3f208a1439c5fc20e1
Dominique Nguyen <domnguyen@gmail.com>
2020-02-03 11:56:02 -0800

    add create a post but has a bug. UID not pulling up

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,15 +1,15 @@
 <template>
-    <div class="dashboard">
+    <div id="dashboard">
         <section>
             <div class="col1">
                 <div class="profile">
-                    <h5>{{userProfile.name}}</h5>
-                    <p>{{userProfile.title}}</p>
+                    <h5>{{ userProfile.name }}</h5>
+                    <p>{{ userProfile.title }}</p>
                     <div class="create-post">
-                        <p>Create Post</p>
+                        <p>create a post</p>
                         <form @submit.prevent>
-                            <textarea></textarea>
-                            <button class="button">post</button>
+                            <textarea v-model.trim="post.content"></textarea>
+                            <button @click="createPost" :disabled="post.content == ''" class="button">post</button>
                         </form>
                     </div>
                 </div>
@@ -24,7 +24,35 @@
 </template>
 
 <script>
-    export default {
-        
+    import { mapState } from 'vuex'
+    const fb = require('../firebaseConfig')
+
+   export default {
+    data() {
+        return {
+            post: {
+                content: ''
+            }
+        }
+    },
+    computed: {
+        ...mapState(['userProfile'])
+    },
+    methods: {
+        createPost() {
+            fb.postsCollection.add({
+                createdOn: new Date(),
+                content: this.post.content,
+                userId: this.currentUser.uid,
+                userName: this.userProfile.name,
+                comments: 0,
+                likes: 0
+            }).then(ref => {
+                this.post.content = ''
+            }).catch(err => {
+                console.log(err)
+            })
+        }   
     }
+}
 </script>
